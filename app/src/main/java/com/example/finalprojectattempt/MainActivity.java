e42b72b692b13ffac0401ec4585c6c4ad66745f8
serens2 <serens2@illinois.edu>
2019-04-23 20:47:02 -0500

    deleted words.xml, added a rook to square A1

--- app/src/main/java/com/example/finalprojectattempt/MainActivity.java
+++ app/src/main/java/com/example/finalprojectattempt/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.finalprojectattempt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 //hello
