36a1f95f3745957ac5ada0abae61668f1c6063ec
serens2 <serens2@illinois.edu>
2019-04-28 20:51:16 -0500

    added code to get the user's location, created Chess.java

--- app/src/main/java/com/example/finalprojectattempt/MainActivity.java
+++ app/src/main/java/com/example/finalprojectattempt/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.finalprojectattempt;
 
+import android.Manifest;
 import android.app.DownloadManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
@@ -19,11 +22,15 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class MainActivity extends AppCompatActivity {
-//hello
+    private static final int REQUEST_COARSE_LOCATION = 88;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +38,42 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView weather = findViewById(R.id.weather);
 
+        class Identity<T> {
+            T t;
+            T get() { return t; }
+            void put(T setT) { t = setT; }
+        }
+
+        boolean canAccessCoarseLocation =
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        if (!canAccessCoarseLocation) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_COARSE_LOCATION);
+        }
+
+        final Identity<Double> latitude = new Identity<>();
+        final Identity<Double> longitude = new Identity<>();
+
+
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            latitude.put(location.getLatitude());
+                            longitude.put(location.getLongitude());
+                        }
+                    }
+                });
+
+        latitude.put(40.10);
+        longitude.put(-88.23);
+
         final String errorMessage = "Sunny and 70 degrees";
-        double latitude = 40.104711;
-        double longitude = -88.2272431;
         String key = "37bfaa968cb61464140b0506164f63f0";
-        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=imperial&APPID=" + key;
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude.get() + "&lon=" + longitude.get() + "&units=imperial&APPID=" + key;
 
         RequestQueue queue = Volley.newRequestQueue(this);
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -46,8 +84,15 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String city = result.get("name").getAsString();
                     String country = result.getAsJsonObject("sys").get("country").getAsString();
-                    String temp = result.getAsJsonObject("main").get("temp").getAsString();
-                    String output = city + ", " + country + "\nTemperature: " + temp + "˚F";
+                    Object description = result.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+                    long temp = Math.round(result.getAsJsonObject("main").get("temp").getAsDouble());
+                    long visibility = Math.round(result.get("visibility").getAsDouble() / 1609);
+                    long pressure = Math.round(result.getAsJsonObject("main").get("pressure").getAsDouble() / 33.86);
+                    String output = city + ", " + country
+                            + "\n" + description
+                            + "\nTemperature: " + temp + "˚F"
+                            + "\nVisibility: " + visibility + " mi"
+                            + "\nPressure: " + pressure + " inHg";
                     weather.setText(output);
                 } catch (Exception e) {
                     weather.setText(errorMessage);
---
+++ lib/src/main/java/com/example/lib/Chess.java
@@ -0,0 +1,60 @@
+package com.example.lib;
+
+/**
+ * Class that represents a game of chess
+ */
+public class Chess {
+    Square[][] board = new Square[8][8];
+}
+
+/**
+ * Abstract class that represents a chess piece
+ */
+abstract class Piece {
+    Color color;
+    Position position;
+    Piece(Color setColor, Position setPosition) {
+        color = setColor;
+        position = setPosition;
+    }
+    void setPosition(Position setPosition) {
+        position = setPosition;
+    }
+}
+
+/**
+ * Class that represents a pawn
+ */
+class Pawn extends Piece {
+    void move() {
+        position.longitude++;
+    }
+    Pawn(Color setColor, Position setPosition) {
+        super(setColor, setPosition);
+    }
+}
+
+/**
+ * Class that represents a position on a chess board
+ */
+class Position {
+    int latitude;
+    int longitude;
+}
+
+/**
+ * Class that represents a square on the chess board
+ */
+class Square {
+    Position position;
+    Piece piece;
+    boolean occupied;
+}
+
+/**
+ * Class that represents the color of a chess piece
+ */
+class Color {
+    public static final Color BLACK = new Color();
+    public static final Color WHITE = new Color();
+}
